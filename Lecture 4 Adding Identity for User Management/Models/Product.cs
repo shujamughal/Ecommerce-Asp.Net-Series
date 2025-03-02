57dc1ed19853188110c331f41053da6bbe77cac4
Imran Javed <bsef21m033@pucit.edu.pk>
2025-03-02 23:44:42 +0500

    Lecture 4

---
+++ Lecture 4 Adding Identity for User Management/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_Based_Learning.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }  // Unique identifier for each product
+        public string Name { get; set; }  // Product name
+        public string Description { get; set; }  // Short product description
+        public decimal Price { get; set; }  // Product price
+        public string ImageUrl { get; set; }  // URL for product image
+    }
+}
