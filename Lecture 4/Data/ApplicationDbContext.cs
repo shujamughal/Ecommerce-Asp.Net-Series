010110eae80d8d764552cf2eb8d31f6c697d4cc7
Imran Javed <bsef21m033@pucit.edu.pk>
2025-03-15 22:56:59 +0500

    Lecture 4
    Lecture 4: Domain Modeling & Entity Framework Core (Code-First Approach)

---
+++ Lecture 4/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project_Based_Learning.Models;
+
+namespace Project_Based_Learning.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure Many-to-Many Relationship
+            modelBuilder.Entity<OrderProduct>()
+                .HasKey(op => new { op.OrderId, op.ProductId });
+        }
+    }
+}
