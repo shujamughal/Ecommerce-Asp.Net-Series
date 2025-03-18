04baedefec2b837921b07d4a289e96b554c35cc0
Imran Javed <bsef21m033@pucit.edu.pk>
2025-03-19 00:14:59 +0500

    Lecture 8: Forms & Validation in ASP.NET Core MVC
    Lecture 8: Forms & Validation in ASP.NET Core MVC

---
+++ Lecture 8/Models/Product.cs
@@ -0,0 +1,49 @@
+﻿using Project_Based_Learning.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_Based_Learning.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }  // Primary Key
+
+        [UniqueProductName]
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(100, ErrorMessage = "Product Name must be between 2 and 100 characters", MinimumLength = 2)]
+        public string Name { get; set; }  // Product Name
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters")]
+        public string Description { get; set; }  // Short Description
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(1, 10000, ErrorMessage = "Price must be between $1 and $10,000")]
+        public decimal Price { get; set; }  // Product Price
+
+        [Required(ErrorMessage = "Image URL is required")]
+        [Url(ErrorMessage = "Invalid image URL format")]
+        public string ImageUrl { get; set; }  // Image Path
+
+        // Foreign Key for Category
+        [Required(ErrorMessage = "Category is required")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }  // Navigation Property
+    }
+
+    public class UniqueProductNameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var _context = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
+            var existingProduct = _context.Products.Any(p => p.Name == value.ToString());
+
+            if (existingProduct)
+            {
+                return new ValidationResult("Product name already exists. Choose a different name.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
