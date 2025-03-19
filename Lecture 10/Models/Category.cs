3803b485f02671f526858bc06b55ccdb4eb6c8d7
Imran Javed <bsef21m033@pucit.edu.pk>
2025-03-19 23:27:45 +0500

    Lecture 10: State Management in ASP.NET Core MVC
    Lecture 10: State Management in ASP.NET Core MVC

---
+++ Lecture 10/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Project_Based_Learning.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }  // Primary Key
+        public string Name { get; set; }  // Category Name
+
+        // Navigation Property for One-to-Many Relationship
+        public ICollection<Product> Products { get; set; }
+    }
+
+}
